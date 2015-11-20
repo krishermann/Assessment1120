1d111e93f269992a5e956ea39456b344bf10df83
Kris Hermann <krishermann@gmail.com>
2015-11-20 14:19:30 -0500

    test

---
+++ assessment.js
@@ -0,0 +1,15 @@
+(function(){
+
+ var animals = [
+{name: “cow”, sound: “moo”},
+{name: “sheep”, sound: “baaah”},
+{name: “dog”, sound: “arf”}
+];
+function seeAndSay(){
+	animals.gray = gray;
+	animals.forEach(being){
+	console.log(“The ” +  being.name +  “says ” +  being.sound + “! And is “ + being.gray);
+};
+}
+seeAndSay();
+})();
